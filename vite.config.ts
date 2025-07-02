b683ff615cfb94403f732f0c9f96d8d59bc95069
SidAndreo122 <sidney122@comcast.net>
2025-07-02 23:09:03 +0000

    Prepare GearFlo for fdeployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/evorg-dotcom/GearFlow_2.git',
   plugins: [react()],
   server: {
     headers: {
